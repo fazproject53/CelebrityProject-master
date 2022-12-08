7c218217a221a268b70208807c821bdbf63c01af
Fayzeh-2019 <Fayzeh_Alnabulsi@hotmail.com>
2022-12-08 10:16:49 +0300

    fayzeh

--- lib/Celebrity/Contracts/contract.dart
+++ lib/Celebrity/Contracts/contract.dart
@@ -340,6 +340,7 @@ int counter = 0;
                                                             temp.contains('اعلان')|| temp.contains('مساحة اعلانية')|| temp.contains('عقد المنصة')?
                                                            setState(() {
                                                              _posts.isNotEmpty?{
+                                                             print('posts not empty when type'),
                                                                help =[],
                                                               help = _posts,
                                                               _posts = []}:help = _postsfilter;
@@ -1546,7 +1547,7 @@ int counter = 0;
    for(int i =0; i< dateChoices.length; i++){
      datetext.keys.contains(dateChoices[i]) ? null:{
        datetext.putIfAbsent(dateChoices[i], ()=> false),
-       datefilter.add(
+       i ==0 || i>2?null:  datefilter.add(
            Directionality(
              textDirection: TextDirection.rtl,
              child: StatefulBuilder(
@@ -1566,7 +1567,9 @@ int counter = 0;
                            borderRadius: BorderRadius.circular(10.r)),
                        child: Padding(
                          padding:  EdgeInsets.symmetric(horizontal: 21.w),
-                         child: text(context, i ==0? 'اليوم': i==1? 'اخر اسبوع':'اخر شهر', 18,  datetext[dateChoices[i]]!?white:blue),
+                         child: text(context, '${Contract
+                             .fromJson(jsonDecode(response.body))
+                             .data!.orders![i].contract!.date}', 18,  datetext[dateChoices[i]]!?white:blue),
                        ),
 
                      ),
